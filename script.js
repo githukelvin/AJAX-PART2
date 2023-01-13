6a8b7d254df7ca6575c3a07b315cf6f33395eb25
githukelvin <kelvingithu019@gmail.com>
2023-01-13 18:15:42 +0300

    Changes are pushed to  branch__2023-01-13_18-15-42

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ btn.addEventListener('click', () => {
     // xhr open connection
     xhr.open("GET", "https://restcountries.com/v3.1/all",true);
     // handling the response
-    xhr.onload = () => {
+    xhr.onload = () => { //ikfyd8yf8dy8f8dy8fy78ydf
         //if the response is successful
         if(xhr.status===200){
             let data = JSON.parse(xhr.responseText);
